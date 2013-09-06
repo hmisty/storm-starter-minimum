a20d3090c8e4ceebb492f43487e93961c73a6f69
Evan Liu <hmisty@gmail.com>
2013-09-06 14:31:23 +0800

    del old

--- src/storm_starter_minimum/core.clj
+++
@@ -1,6 +0,0 @@
-(ns storm-starter-minimum.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- test/storm_starter_minimum/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns storm-starter-minimum.core-test
-  (:use clojure.test
-        storm-starter-minimum.core))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
