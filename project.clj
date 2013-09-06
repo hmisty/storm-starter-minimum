67cf7ee7e1e9706fd1399e0891df1f2761891234
Evan Liu <hmisty@gmail.com>
2013-09-06 22:03:09 +0800

    workable word_count

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject storm-starter-minimum "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :source-paths ["src/clj"]
+  :aot :all
+  :dependencies []
+  :profiles {:dev
+             {:dependencies [[storm "0.8.2"]
+                             [org.clojure/clojure "1.5.1"]]}})
